659304b3227cabc391c1d39230fdfce47a107b2b
Paul Hickey <pth@structure101.com>
2013-12-30 19:26:46 +0100

    First commit
    Signed-off-by: Paul Hickey <pth@structure101.com>

---
+++ about/index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>La Maison des Amis - Family holiday rental in the Heart of the Tarn, France</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="https://netdna.bootstrapcdn.com/bootstrap/2.3.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/2.3.2/css/bootstrap-responsive.css" rel="stylesheet">
+    <link href="https://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
+  </head>
+
+  <body>
+
+
+
+    <!-- Include NAVBAR
+    ================================================== -->
+	<?php include($_SERVER['DOCUMENT_ROOT'] . "/includes/nav.html"); ?>
+
+    <div class="container about">    
+		<div class="row">
+			<div class="span5">
+		
+				<h1>About us</h1>
+			</div>
+		</div><!-- /.row -->
+		<div class="row">
+			<div class="span6">
+				<p>We are Paul, Nadia, Samuel, Lochlann, Darrach and Thomas, &#8220;une famille recomposée&#8221;.</p>
+				<p>We have traveled extensively throughout the world and have setup La Maison des Amis to cater primarily for families, with all the comforts of home.</p>
+				<p>Paul has worked in the software industry since 1986, and is a founder at <a href="http://structure101.com" title="structure101.com">Structure101</a>.</p>
+				<p>Nadia previously worked as a tour guide in South America and now teaches Spanish at the lycée in Gaillac.</p>
+				<p>Samuel, Lochlann, Darrach and Thomas are all at collège.  When they can drag themselves away from the PlayStation, they play drums, handball, football and tennis.</p>
+				<p>When we are not taxi-ing the boys around, we are happy to do what&#8217;s necessary to make your stay more comfortable.</p>
+			</div><!-- /.span6 -->
+			<div class="span6">
+				<img height="" width="" src="/images/paul-and-nadia.jpg" >
+			</div><!-- /.span6 -->
+		</div><!-- /.row -->
+		<div class="row">
+			<div class="span6 ">
+				<img class="" height="" width="" src="/images/boys-in-gallicia.jpg" >
+			</div><!-- /.span6 -->
+			<div class="span3">
+				<h2 class="featurette-heading">Contact <span class="muted">us</span></h2>
+				<p class="lead"><i class="fa fa-mobile"></i> +33 6 13 33 65 33 (Nadia)</p>
+				<p class="lead"><i class="fa fa-mobile"></i> +33 6 18 51 23 85 (Paul)</p>
+				<p class="lead"><i class="fa fa-phone"></i> +33 5 63 41 87 32</p>
+				<p class="lead"><i class="fa fa-envelope"></i> <a href="mailto:info@darrach.net">info@darrach.net</a></p>
+			</div><!-- /.span3 -->
+		</div><!-- /.row -->
+		<!-- FOOTER -->
+		<footer>
+		<hr/>
+		  <p class="pull-right"><a href="#">Back to top</a></p>
+		  <p>La Maison des Amis - In the Heart of the Tarn</p>
+		</footer>
+	</div><!-- /.container -->
+
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.js"></script>
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js"></script>
+    <script>
+      !function ($) {
+        $(function(){
+          // carousel demo
+          $('#myCarousel').carousel()
+        })
+      }(window.jQuery)
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/holder/2.0/holder.js"></script>
+  </body>
+</html>
