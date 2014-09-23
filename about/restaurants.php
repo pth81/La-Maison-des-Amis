fb852ea8921bbf01937ba3a789ef3621ec0fdcfd
Paul Hickey <pth81500@gmail.com>
2014-09-23 21:57:07 +0200

    Added reasonable page titles

--- about/restaurants.php
+++ about/restaurants.php
@@ -5,7 +5,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <title>La Maison des Amis - Family holiday rental in the Heart of the Tarn, France</title>
+    <title>La Maison des Amis - Local Restaurants - Giroussens, Lavaur, Ambres</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
