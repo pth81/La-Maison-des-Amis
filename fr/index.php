a709065a5da38a90931b75ef131f9f227ea87636
Paul Hickey <pth81500@gmail.com>
2014-09-24 22:22:26 +0200

    A shit load of changes following a Deeptrawl session

--- fr/index.php
+++ fr/index.php
@@ -5,9 +5,11 @@
 <html lang="fr">
 	<head>
 		<meta charset="utf-8">
-		<title>La Maison des Amis - Gîte avec Piscine et wifi dans Le Tarn. Giroussens, 10 KM de Lavaur, Rabastens, St Sulpice.</title>
+		<title>La Maison des Amis - Gîte, piscine & wifi. Giroussens, Lavaur, Tarn.</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="">
+		<meta name="description" content="La Maison des Amis - grande gîte dans un parc de 2 hectares, avec piscine & wifi a louer. Giroussens, Tarn, moins de 10 Km de Lavaur, Rabastens et St. Sulpice. 10 personnes.">
+		<meta name="keywords" content="vacances, gîte, maison, location, louer, piscine, tennis, jardin, Giroussens, Lavaur6, Tarn, France" />
+		
 		
 		<!-- Include Common Header 
 		================================================== -->
--- index.php
+++ index.php
@@ -5,9 +5,10 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>La Maison des Amis - Family holiday villa rental with pool & wifi in the Heart of the Tarn, France</title>
+		<title>La Maison des Amis - Family holiday villa with pool & wifi, Tarn, France</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="">
+		<meta name="description" content="Large 4-bedroom holiday villa in the Tarn, France. A beautifully converted barn on 5 acres, with pool, tennis and wifi. Comfortably sleeps 10.">
+		<meta name="keywords" content="holiday, vacation, villa, house, rental, pool, tennis, park, garden, Lavaur, Tarn, France" />
 		
 		<!-- Include Common Header 
 		================================================== -->
